006574580cbc78cbdee31ca6d4fe877063ceaecc
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 18:14:04 +0100

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,1 +1,12 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-100 py-6 mt-10">
+      <div className="max-w-7xl mx-auto text-center text-sm text-gray-500">
+        © {new Date().getFullYear()} ListingApp. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
 
