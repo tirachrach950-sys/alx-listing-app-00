a4e53fc9e28092ec155e9cde52bfac3b9d29461e
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 18:11:14 +0100

    Update index.ts

--- components/layout/interfaces/index.ts
+++ components/layout/interfaces/index.ts
@@ -1,1 +1,32 @@
+export interface AddressProps {
+  state: string;
+  city: string;
+  country: string;
+}
+
+export interface OffersProps {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: AddressProps;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: OffersProps;
+  image: string;
+  discount: string;
+}
+
+export interface PillProps {
+  title: string;
+}
+
+export interface ButtonProps {
+  title: string;
+  styles: string;
+}
 
