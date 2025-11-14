721f5f99d0dca0d352c10c3ebf55b689b751c7e4
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 18:14:23 +0100

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,1 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<any> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen pt-6">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
 
