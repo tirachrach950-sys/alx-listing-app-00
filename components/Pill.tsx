1dc75f8694599eefcc777dbecc9e202e5c5f7791
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 18:16:01 +0100

    Update Pill.tsx

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,1 +1,12 @@
+import { PillProps } from "@/interfaces";
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="px-4 py-2 bg-gray-200 rounded-full text-xs cursor-pointer hover:bg-gray-300">
+      {title}
+    </div>
+  );
+};
+
+export default Pill;
 
